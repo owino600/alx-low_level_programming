b946f600f4d20a7cde4998e9b78c70a5b3148cee
owino600 <owinodaniel600@gmail.com>
2023-06-15 05:44:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #!include<stdio.h>
 /""
-*main - A program that print a line using printf function
-*Return Aiways 0(success)
-*/
+ * main - A program that print a line using printf function
+ * Return: Aiways 0(success)
+ */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
