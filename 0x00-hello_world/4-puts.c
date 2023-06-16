414f8e2e9a764246fed1cb9271ac17de45f4b1ce
owino600 <owinodaniel600@gmail.com>
2023-06-16 04:51:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include(stdio.h)
+#include <stdio.h>
 /**
- * main - Entry point
+ *Main - A processor that prints a line with puts function
  *
- * Return: Always 0(success)
+ *Return: always 0(Success)
  */
-int main (void)
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
