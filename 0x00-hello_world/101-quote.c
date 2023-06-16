d2aa3a96595180cf80865544eeeb2c3cf64b15a5
owino600 <owinodaniel600@gmail.com>
2023-06-16 05:20:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
-*man-Entry point
-*
-*Return Always 1(Success)
-*/
+ * Main - A program that prints lines using write function
+ * Return: Always 1(success)
+ */
 int main(void)
 {
-write(2,
-"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
