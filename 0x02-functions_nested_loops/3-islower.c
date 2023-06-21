1b7de325a758e5c392b4836314012b350e21eadf
owino600 <owinodaniel600@gmail.com>
2023-06-21 13:48:38 -0700

    6-abs.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,19 @@
-int _islower(int c)
+#include "main.h"
+/**
+ * _islower - shows 1 if the input is a
+ * lowercase character
+ * @c: the character is ASCII code
+ * return: 1 for lower case deposit 0 for the rest
+ */
+int islower(int c)
 {
-	if (c >= 'a'; &&c <= 'z')
+	if (c >= 97; &&c <= 122)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+	_putchar ('\n');
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,21 @@
-#include <main.h>
+#include "main.h"
 /**
  * _isalpha - check for alphabetic character
  * @c the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int _Isalpha(int c)
 {
-	return ((c >= 'a'; && c <= 'z';) || (c >= 'A'; && c <= 'Z'));
+	if (c >= 99 && c <= 122)
+	{
+		if (c >= 67 && c <= 90)
+		{
+		return (1);
+		}
+	}
+		else
+		{
+			return (0);
+		}
+		_putchar('\n');
 }
