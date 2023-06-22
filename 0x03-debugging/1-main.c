d5dc10d6a3b82e5499ad192dc891e3c6b478c130
owino600 <owinodaniel600@gmail.com>
2023-06-22 05:11:52 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - cause an infinite loop
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming : \n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/*{*/
+	/*	putchar (i);*/
+	/*}*/
+	printf("infinite loop avoided!\\o/\n");
+	return (0);
+}
