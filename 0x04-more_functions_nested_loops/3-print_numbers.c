b6fb923a923044f3973649b0f1c17bd805e60ab5
owino600 <owinodaniel600@gmail.com>
2023-06-22 07:38:11 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_number - print 0-9
+ * return: void
+ */
+void print_number(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
