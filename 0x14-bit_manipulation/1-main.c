9573c7425fb70a3c68d6f4b7cf356bf80b8733e0
owino600 <2203655@students.kcau.ac.ke>
2023-08-31 12:40:19 -0700

    update

---
+++ 0x14-bit_manipulation/1-main.c
@@ -0,0 +1,23 @@
+julien@ubuntu:~/0x14. Binary$ cat 1-main.c 
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_binary(0);
+    printf("\n");
+    print_binary(1);
+    printf("\n");
+    print_binary(98);
+    printf("\n");
+    print_binary(1024);
+    printf("\n");
+    print_binary((1 << 10) + 1);
+    printf("\n");
+    return (0);
+}
