77ed0aabf0000be176f306a70d2faae1ab15e106
owino600 <owinodaniel600@gmail.com>
2023-12-28 23:00:50 +0300

    updates

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Define an island perimeter measuring function"""
+def island_perimeter(grid):
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                    if (1 > 0 and grid[i -1][j] == 1):
+                        edge += 1
+                        return size * 4 - edges * 2
