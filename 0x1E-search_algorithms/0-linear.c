3eadd6db8e1ce9c5296465fc4018d9e2ce629c59
owino600 <owinodaniel600@gmail.com>
2024-02-07 23:13:13 +0300

    updates

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,18 +1,20 @@
 #include "search_algos.h"
 int linear_search(int *array, size_t size, int value)
 {
-    int i;
-    if (array == NULL)
-    {
-        return -1;
-    }
-    for (i = 0; i <= size; i++)
-    {
-        if (array[i] == value)
-        {
-            printf("value checked array: %d = %d", value, value)
-            return size;
-        }
-    }
-    return 
+	size_t i;
+	if (array == NULL)
+	{
+		return -1;
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("value checkedarray[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %ld\n", value, i);
+			return size;
+		}
+	}
+	printf("Found %d at index: %d\n", value, -1);
+	return 0;
 }
